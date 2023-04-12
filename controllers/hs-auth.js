84474204edc09f81146dac454ef5dbe4b78bc348
CodeSevenic <sevenicbookings@gmail.com>
2023-04-12 14:43:38 +0200

    Hub-Nabled

--- controllers/hs-auth.js
+++ controllers/hs-auth.js
@@ -1,6 +1,6 @@
 ﻿exports.hsAuth = (req, res) => {
   const clientId = process.env.APP1_CLIENT_ID;
-  const scope = 'crm.objects.contacts.read';
+  const scope = process.env.APP1_SCOPES;
   const redirectUri = process.env.HUBSPOT_REDIRECT_URI;
 
   const authUrl = `https://app.hubspot.com/oauth/authorize?client_id=${clientId}&scope=${scope}&redirect_uri=${redirectUri}`;
