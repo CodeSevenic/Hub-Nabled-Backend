b26ad8b62760b8674eb0b08eabb198b715e7485d
CodeSevenic <sevenicbookings@gmail.com>
2023-04-12 17:00:54 +0200

    Hub-Nabled

--- config/index.js
+++ config/index.js
@@ -2,14 +2,16 @@
 
 dotenv.config();
 
+const PORT = process.env.PORT || 8000;
+
 const config = {
   CLIENT_ID: process.env.APP1_CLIENT_ID,
   CLIENT_SECRET: process.env.APP1_CLIENT_SECRET,
   SCOPES: process.env.APP1_SCOPES
     ? process.env.APP1_SCOPES.split(/ |, ?|%20/).join(' ')
     : ['crm.objects.contacts.read'],
-  PORT: process.env.PORT || 4000,
-  REDIRECT_URI: `http://localhost:${PORT}/api/v1/callback`,
+  PORT: PORT,
+  REDIRECT_URI: `http://localhost:${PORT}/oauth-callback`,
 };
 
 if (!config.CLIENT_ID || !config.CLIENT_SECRET) {
