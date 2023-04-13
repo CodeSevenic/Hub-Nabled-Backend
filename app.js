b490db4c624d5ba6493c30d5eb475efa255ae22c
CodeSevenic <sevenicbookings@gmail.com>
2023-04-13 13:11:11 +0200

    Hub-Nabled

--- app.js
+++ app.js
@@ -15,6 +15,33 @@ app.use(
   })
 );
 
+// API route for user registration
+app.post('/api/register', async (req, res) => {
+  // Implement user registration logic here
+  // For example, you can store user information in Firebase
+  res.status(200).json({ message: 'User registered successfully' });
+});
+
+// API route for user login
+app.post('/api/login', async (req, res) => {
+  // Implement user login logic here
+  // For example, you can retrieve user information from Firebase and validate the credentials
+  res.status(200).json({ message: 'User logged in successfully' });
+});
+
+// API route for installing an app
+app.get('/api/install', hubspot.handleInstall(hubspot.authUrl));
+
+// API route for handling OAuth callback
+app.get('/api/oauth-callback', hubspot.handleOauthCallback);
+
+// API route for checking user authorization
+app.get('/api/authorized', async (req, res) => {
+  const userId = req.query.userId;
+  const authorized = await hubspot.isAuthorized(userId);
+  res.status(200).json({ authorized });
+});
+
 app.use('/', indexRoutes);
 app.use('/', authRoutes);
 
