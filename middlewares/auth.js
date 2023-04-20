cc526f23a2cf99650df10f6edfe2cf068dde70fc
CodeSevenic <sevenicbookings@gmail.com>
2023-04-20 10:20:03 +0200

    Hub Nabled Code update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,25 +7,29 @@ const authMiddleware = async (req, res, next) => {
     // Use the imported isAuthorized function here
     next();
   } else {
+    res.status(401).json({
+      message: 'You are not authorized',
+    });
     // Send an HTML response with an Install button
-    res.send(`
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Not Authorized</title>
-      </head>
-      <body>
-          <h1>You are not authorized</h1>
-          <p>
-              <a href="/api/install">
-                  <button>Install</button>
-              </a>
-          </p>
-      </body>
-      </html>
-    `);
+
+    // res.send(`
+    //   <!DOCTYPE html>
+    //   <html lang="en">
+    //   <head>
+    //       <meta charset="UTF-8">
+    //       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //       <title>Not Authorized</title>
+    //   </head>
+    //   <body>
+    //       <h1>You are not authorized</h1>
+    //       <p>
+    //           <a href="/api/install">
+    //               <button>Install</button>
+    //           </a>
+    //       </p>
+    //   </body>
+    //   </html>
+    // `);
   }
 };
 
