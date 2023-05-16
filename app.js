636c75c9878aa2640bba539af905eb4a034ce9ff
CodeSevenic <sevenicbookings@gmail.com>
2023-05-16 10:20:23 +0200

    Code Updater

--- app.js
+++ app.js
@@ -39,10 +39,16 @@ app.use(
 app.post('/apps', async (req, res) => {
   const appData = req.body;
 
+  // Make sure appName is provided
+  if (!appData.appName) {
+    return res.status(400).json({ message: 'appName is required' });
+  }
+
   try {
-    const docRef = await db.collection('apps').add(appData);
-    console.log('Document written with ID: ', docRef.id);
-    res.status(201).json({ id: docRef.id });
+    const docRef = db.collection('apps').doc(appData.appName);
+    await docRef.set(appData);
+    console.log('Document written with ID: ', appData.appName);
+    res.status(201).json({ id: appData.appName });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'An error occurred while saving the app' });
