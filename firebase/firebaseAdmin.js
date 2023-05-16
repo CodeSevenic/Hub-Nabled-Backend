49be1739d9dd23ad67cb1318ab2d413111d60429
CodeSevenic <sevenicbookings@gmail.com>
2023-05-16 08:46:21 +0200

    Code Updater

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -1,6 +1,6 @@
 ﻿var admin = require('firebase-admin');
 
-var serviceAccount = require('../hub-nabled-firebase-adminsdk-z9tzu-4de95eb8bc.json');
+var serviceAccount = require('../env/hub-nabled-firebase-adminsdk-z9tzu-4de95eb8bc.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
