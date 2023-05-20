fe25e677919df06f7ca3b22561df7df50e2c097e
CodeSevenic <sevenicbookings@gmail.com>
2023-05-20 15:31:15 +0200

    Hub Nabled Code update

--- controllers/apps.js
+++ controllers/apps.js
@@ -1,6 +1,6 @@
 ﻿const { db } = require('../firebase/firebaseAdmin');
 
-// function to add HubSpot apps to Firebase
+// function to add HubSpot app to Firebase
 exports.addApp = async (req, res) => {
   const appData = req.body;
 
@@ -17,5 +17,22 @@ exports.addApp = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'An error occurred while saving the app' });
+  }
+};
+
+// function to get HubSpot by appName from Firebase
+exports.getApp = async (req, res) => {
+  const { appName } = req.params;
+
+  try {
+    const doc = await db.collection('apps').doc(appName).get();
+    if (!doc.exists) {
+      res.status(404).json({ message: `App with name ${appName} not found` });
+    } else {
+      res.status(200).json({ id: doc.id, ...doc.data() });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'An error occurred while fetching the app' });
   }
 };
