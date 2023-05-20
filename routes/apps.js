82a9dbd437b2deaca05e9fb2df9ca43cf912893f
CodeSevenic <sevenicbookings@gmail.com>
2023-05-20 15:31:22 +0200

    Hub Nabled Code update

--- routes/apps.js
+++ routes/apps.js
@@ -1,9 +1,12 @@
 ﻿const express = require('express');
-const { addApp } = require('../controllers/apps');
+const { addApp, getApp } = require('../controllers/apps');
+const { get } = require('request');
 
 const router = express.Router();
 
 // API route to Add HubSpot apps to Firebase
 router.post('/apps', addApp);
+// API route to get HubSpot app by appName from Firebase
+router.get('/apps/:appName', getApp);
 
 module.exports = router;
