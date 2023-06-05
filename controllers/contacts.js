e1d0366fd2545248f1a05701ebbfd13a08b2d3b7
CodeSevenic <sevenicbookings@gmail.com>
2023-06-05 13:02:51 +0200

    Hub Nabled Updates 23 May 2023

---
+++ controllers/contacts.js
@@ -0,0 +1,9 @@
+﻿const { getAccessToken, getContact } = require('../services/hubspot');
+
+exports.contacts = async (req, res) => {
+  const accessToken = await getAccessToken(req.sessionID);
+  const contact = await getContact(accessToken);
+  const { firstname, lastname } = contact.properties;
+  console.log(firstname, lastname);
+  res.write(`<p>Contact name: ${firstname.value} ${lastname.value}</p>`);
+};
---
+++ routes/contacts.js
@@ -0,0 +1,13 @@
+﻿const express = require('express');
+const router = express.Router();
+
+const isAuthorized = require('../middlewares/auth');
+const { contacts } = require('../controllers/contacts');
+
+router.get('/contacts', isAuthorized, contacts);
+
+router.get('/error', (req, res) => {
+  res.render('error', { msg: req.query.msg });
+});
+
+module.exports = router;
