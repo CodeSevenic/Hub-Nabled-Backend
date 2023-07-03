46e27051ab90187ec835e9ed9b787ccb79ffabbf
CodeSevenic <sevenicbookings@gmail.com>
2023-07-03 08:48:42 +0200

    Hub-Nabled

--- plugins/fetch-contact.js
+++ plugins/fetch-contact.js
@@ -1,4 +1,5 @@
 ﻿const { default: axios } = require('axios');
+const { getAccessToken, isAuthorized } = require('../services/hubspot');
 
 // Define an async function to get contacts
 const fetchContacts = async (accessToken) => {
@@ -44,6 +45,30 @@ const fetchContacts = async (accessToken) => {
   return allContacts;
 };
 
+exports.fetchHubSpotContacts = async (req, res) => {
+  const userId = req.session.userId;
+
+  const portalId = req.params.portalId ? req.params.portalId : req.session.portalId;
+
+  console.log('Portal ID: ', portalId);
+
+  const authorized = await isAuthorized(userId, portalId);
+  console.log('Authorized: ', authorized);
+
+  if (authorized) {
+    const accessToken = await getAccessToken(userId, portalId);
+
+    const contacts = await fetchContacts(accessToken);
+
+    res.json(contacts);
+  } else {
+    console.log('User is not authorized or has not installed an app');
+    res.status(401).json({
+      message: 'You are not authorized or has not installed an app',
+    });
+  }
+};
+
 module.exports = {
-  fetchContacts,
+  fetchHubSpotContacts,
 };
