d08bf6db2fb97d060c1afc0117a9a320f0ea2a5f
CodeSevenic <sevenicbookings@gmail.com>
2023-07-03 09:19:11 +0200

    Hub-Nabled

--- routes/pgn-executer.js
+++
@@ -1,7 +0,0 @@
-﻿const express = require('express');
-const router = express.Router();
-const { featureToggle } = require('../controllers/features');
-
-router.post('/toggle-feature', featureToggle);
-
-module.exports = router;
---
+++ routes/pgnExecuter.js
@@ -0,0 +1,7 @@
+﻿const express = require('express');
+const { pluginExecuter } = require('../controllers/pgnExecuter');
+const router = express.Router();
+
+router.post('/execute-feature/:userId/:hubspotId/:featureName', pluginExecuter);
+
+module.exports = router;
