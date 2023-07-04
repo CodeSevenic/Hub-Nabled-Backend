b800fc9a7e14f59e8d19cb58e40bfcccaab601cc
CodeSevenic <sevenicbookings@gmail.com>
2023-07-04 07:38:39 +0200

    Hub-Nabled

--- plugins/functions/features.js
+++ plugins/functions/features.js
@@ -7,7 +7,26 @@ const featuresList = {
   contacts: {
     name: 'Fetch Hubspot Contacts',
     function: fetchHubSpotContacts,
-    description: "Fetches contacts from a user's Hubspot account",
+    description: "Fetches contacts from a user's HubSpot account",
+    featureId: 'contacts',
+  },
+  contactFormatter: {
+    name: 'Hubspot Contact Formatter',
+    function: fetchHubSpotContacts,
+    description: "Formats contacts from a user's HubSpot account",
+    featureId: 'contactFormatter',
+  },
+  deals: {
+    name: 'Deals Explorer',
+    function: fetchHubSpotContacts,
+    description: "Fetches deals from a user's HubSpot account",
+    featureId: 'deals',
+  },
+  lists: {
+    name: 'List View',
+    function: fetchHubSpotContacts,
+    description: "Fetches lists from a user's HubSpot account",
+    featureId: 'lists',
   },
   // Add other feature-function mappings here...
 };
@@ -18,6 +37,12 @@ exports.getAllFeatures = () => {
 
 exports.getEnabledFeatures = async (userId, hubspotId) => {
   const userFeatures = await getUserFeatures(userId, hubspotId);
+
+  // If user does not have any features, return an empty array
+  if (!userFeatures) {
+    return [];
+  }
+
   const enabledFeatureKeys = Object.keys(userFeatures).filter((feature) => userFeatures[feature]);
 
   const enabledFeatures = enabledFeatureKeys.map((featureKey) => {
