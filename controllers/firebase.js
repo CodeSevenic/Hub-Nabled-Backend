d2ca958c5e58ce0e5449fa5e55e1ef6e3ece9195
CodeSevenic <sevenicbookings@gmail.com>
2023-07-05 12:55:40 +0200

    Hub-Nabled

--- controllers/firebase.js
+++ controllers/firebase.js
@@ -1,15 +1,14 @@
 ﻿const { deleteUserAppAuth } = require('../firebase/firebaseAdmin');
 
 exports.deleteUserAccountAuth = async (req, res) => {
-  const { userId } = req.params;
-  const { portalId } = req.body;
+  const { userId, portalId } = req.params;
 
   try {
     await deleteUserAppAuth(userId, portalId);
 
     res.status(200).json({
       status: 'success',
-      message: `App ${appId} has been deleted successfully.`,
+      message: `App ${portalId} has been deleted successfully.`,
     });
   } catch (error) {
     res.status(500).json({ status: 'error', message: error.message });
