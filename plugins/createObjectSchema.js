aea8ee8153359c90650943a03211151ac8e0afb9
CodeSevenic <sevenicbookings@gmail.com>
2023-07-06 06:07:26 +0200

    Hub-Nabled

--- plugins/createObjectSchema.js
+++ plugins/createObjectSchema.js
@@ -1,8 +1,12 @@
-﻿const createObjectSchema = async (userId, portalId, request) => {
-  const ObjectSchemaEgg = request.body.schema;
+﻿const { default: axios } = require('axios');
+const { isAuthorized, getAccessToken } = require('../services/hubspot');
+
+const createObjectSchema = async (userId, portalId, request) => {
+  const ObjectSchemaEgg = request.body;
   try {
     const authorized = await isAuthorized(userId, portalId);
     if (authorized) {
+      const accessToken = 'pat-eu1-9b9c5964-c226-4c3b-9d85-f6ee952b44d7';
       const response = await axios({
         method: 'post',
         url: `https://api.hubapi.com/crm/v3/schemas`,
