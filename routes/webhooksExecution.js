604d3f01b0a292031159d31fa9f6d34608e40bbd
CodeSevenic <sevenicbookings@gmail.com>
2023-07-08 12:50:24 +0200

    Hub-Nabled

--- routes/webhooksExecution.js
+++ routes/webhooksExecution.js
@@ -1,5 +1,5 @@
 ﻿const express = require('express');
-const { webhookEvents } = require('../services/hubspot/app-wehooks');
+const { webhookEvents } = require('../services/hubspot/wehookEvents');
 const router = express.Router();
 
 router.post('/hubspot-portal/event', webhookEvents);
