3bfa0b4045b1b29f5bcc7a88f82eebfb784bcde4
CodeSevenic <sevenicbookings@gmail.com>
2023-07-18 11:48:36 +0200

    Hub-Nabled

---
+++ utils/sendgrid.js
@@ -0,0 +1,56 @@
+﻿const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+function sendEmail({ to, subject, templateId, dynamicTemplateData }) {
+  const msg = {
+    to,
+    from: process.env.SENDGRID_EMAIL, // Change to your verified sender
+    subject,
+    templateId,
+    dynamicTemplateData,
+  };
+
+  sgMail
+    .send(msg)
+    .then(() => console.log('Email sent'))
+    .catch((error) => console.error(error));
+}
+
+function sendWelcomeEmail(to) {
+  sendEmail({
+    to,
+    subject: 'Welcome!',
+    templateId: 'your_welcome_template_id',
+    dynamicTemplateData: {
+      /* Your dynamic data for template */
+    },
+  });
+}
+
+function sendErrorLogEmail(to, err) {
+  sendEmail({
+    to,
+    subject: 'Error Log',
+    templateId: 'your_error_template_id',
+    dynamicTemplateData: {
+      error: err.stack,
+    },
+  });
+}
+
+function sendMonthlyCheckupEmail(to) {
+  sendEmail({
+    to,
+    subject: 'Monthly Checkup',
+    templateId: 'your_monthly_checkup_template_id',
+    dynamicTemplateData: {
+      /* Your dynamic data for template */
+    },
+  });
+}
+
+module.exports = {
+  sendWelcomeEmail,
+  sendErrorLogEmail,
+  sendMonthlyCheckupEmail,
+};
