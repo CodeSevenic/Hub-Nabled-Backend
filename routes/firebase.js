b2af1d768ce10988bb2e871360e94e87d823d2af
CodeSevenic <sevenicbookings@gmail.com>
2023-07-20 14:46:14 +0200

    Hub-Nabled

--- routes/firebase.js
+++ routes/firebase.js
@@ -1,7 +1,5 @@
 ﻿const express = require('express');
 const router = express.Router();
-const { handleInstall, handleOauthCallback } = require('../services/hubspot');
-const { register, login, logout, getUserAuths } = require('../controllers/auth');
 const { deleteUserAccountAuth } = require('../controllers/firebase');
 
 // API route for user registration
