dea9907049211deadd6bbf4ab2927e0bda7ee8fd
CodeSevenic <sevenicbookings@gmail.com>
2023-07-25 21:50:06 +0200

    Hub-Nabled

--- app.js
+++ app.js
@@ -5,28 +5,34 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { default: axios } = require('axios');
 const { sendWelcomeEmail } = require('./utils/sendgrid');
+const path = require('path');
 const app = express();
-const { PORT } = require('./config');
+const { PORT, NODE_ENV } = require('./config');
+
+console.log('NODE_ENV: ', NODE_ENV);
 
 // Set CORS headers
-app.use(
-  cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'], // Replace with your frontend origin
-    methods: ['GET', 'POST'],
-    credentials: true,
-  })
-);
-app.use(express.json({ limit: '50mb' }));
+if (NODE_ENV === 'DEV') {
+  app.use(
+    cors({
+      origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'], // Replace with your frontend origin
+      methods: ['GET', 'POST'],
+      credentials: true,
+    })
+  );
+}
+
+// app.use(express.json({ limit: '50mb' }));
 
 app.use(cookieParser());
 // Use a session to keep track of client ID
 app.use(
   session({
     secret: process.env.SESSION_SECRET, // This should be a long and complex string
-    resave: false,
+    resave: NODE_ENV === 'PROD', // Secure in PROD, not in DEV
     saveUninitialized: true,
     cookie: {
-      secure: false,
+      secure: NODE_ENV === 'PROD', // Secure in PROD, not in DEV
       maxAge: 1000 * 60 * 60 * 24, // 1 day
     },
   })
@@ -46,6 +52,17 @@ app.use('/api/', featuresRoutes);
 app.use('/api/', pluginExecutionRoutes);
 app.use('/api/', firebase);
 app.use('/api/', hubSpotWebhooks);
+
+// Front End
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', function (_, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
 
 app.listen(PORT, () => console.log(`=== Starting your app on http://localhost:${PORT} ===`));
 // opn(`http://localhost:${PORT}/api/`);
